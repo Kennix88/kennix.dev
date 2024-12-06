113a7953748f150340f8173d49e110329bb7997d
Kennix88 <shel.live@yandex.ru>
2024-12-06 18:51:53 +0800

    update

--- src/app/(public)/_components/Ads.tsx
+++ src/app/(public)/_components/Ads.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 export default function Ads() {
   return (
     <div>
@@ -14,6 +16,40 @@ export default function Ads() {
             overflow: 'hidden',
             backgroundColor: 'transparent',
           }}></iframe>
+        <iframe
+          data-aa="2369010"
+          src="//ad.a-ads.com/2369010?size=300x600"
+          style={{
+            width: 300,
+            height: 600,
+            border: 0,
+            padding: 0,
+            overflow: 'hidden',
+            backgroundColor: 'transparent',
+          }}></iframe>
+        <Script id="atADS" type="text/javascript">
+          {`atOptions = {
+            'key': '78fd6fef97e158e801e0fc1e6c487594',
+            'format': 'iframe',
+            'height': 250,
+            'width': 300,
+            'params': {},
+          };`}
+        </Script>
+        <Script
+          id="atADS2"
+          type="text/javascript"
+          src="//www.highperformanceformat.com/78fd6fef97e158e801e0fc1e6c487594/invoke.js"></Script>
+
+        <div id="yandex_rtb_R-A-13228701-1" className="w-full h-[400px]"></div>
+        <Script id="yaContextS">
+          {`window.yaContextCb.push(() => {
+            Ya.Context.AdvManager.render({
+              blockId: 'R-A-13228701-1',
+              renderTo: 'yandex_rtb_R-A-13228701-1',
+            })
+          })`}
+        </Script>
       </div>
     </div>
   )
